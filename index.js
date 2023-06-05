d13dd511ba2b11a43f53bed7feeac0b4c4eb64c4
Brody Blair <brocbla@umich.edu>
2023-06-04 22:00:03 -0500

    added pawn movement and pawn movement getter.

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ class Board {
     for (let i = 0; i < Board.LANE_SIZE; i++) {
       const p = new Pawn("white", 1, i);
       this.#board[p.row][p.file] = p;
-      const pB = new Pawn("Black", 6, i)
-      this.#board[pB.row][pB.file] = pB
-      console.log(pB)
+      const pB = new Pawn("Black", 6, i);
+      this.#board[pB.row][pB.file] = pB;
+      console.log(pB);
     }
-    console.log(this.#board)
+    console.log(this.#board);
   }
   getSquareContent(row, file) {
     return this.#board[row][file];
@@ -31,12 +31,24 @@ class Board {
 
 class Piece {
   name = null;
+
+  constructor(board, color) {
+    this.color = color;
+    this.board = board;
+  }
+
   toString() {
     return `<${this.name}>`;
   }
-  constructor(color) {
-    this.color = color;
+
+  isBlack() {
+    return this.color === "black";
   }
+
+  isWhite() {
+    return this.color === "white";
+  }
+
   get moves() {
     throw new Error("moves getter is not yet implementeed");
   }
@@ -44,16 +56,32 @@ class Piece {
 
 class Pawn extends Piece {
   name = "Pawn";
-  constructor(color, row, file) {
-    super(color);
+
+  constructor(board, color, row, file) {
+    super(board, color);
     this.row = row;
     this.file = file;
     this.hasMoved = false;
   }
+
+  isSquareOccupied(row, file) {
+    let targetMove = [row, file];
+    let squareContent = this.board.getSquareContent(...targetMove);
+    return squareContent !== null;
+  }
+
   get moves() {
     const available = [];
-    available.push([this.row + 1, this.file]);
-    console.log("small change")
+    const direction = this.isWhite() ? 1 : -1;
+    const firstSquare = [this.row + 1 * direction, this.file];
+    if (!this.isSquareOccupied(...firstSquare)) {
+      available.push(firstSquare);
+    }
+    const secondSquare = [this.row + 2 * direction, this.file];
+    if (!this.hasMoved && !this.isSquareOccupied(...secondSquare)) {
+      available.push(secondSquare);
+    }
+
     return available;
   }
 }
@@ -64,7 +92,7 @@ const b = new Board();
 
 b.initialize();
 
-const p = new Pawn("white", 1, 0);
+const p = new Pawn(b, "white", 1, 0);
 
 console.log(Board.LANE_SIZE);
 
