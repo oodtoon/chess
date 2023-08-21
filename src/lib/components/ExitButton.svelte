e0ce56d0e06191815ecec04e3d2defb184fdf61d
Brody Blair <brocbla@umich.edu>
2023-08-21 18:04:26 -0500

    refactored icons into own components for easy reading

---
+++ src/lib/components/ExitButton.svelte
@@ -0,0 +1,23 @@
+<button class="exit">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="32"
+    viewBox="0 0 1024 1024"
+    ><path
+      fill="currentColor"
+      d="M195.2 195.2a64 64 0 0 1 90.496 0L512 421.504L738.304 195.2a64 64 0 0 1 90.496 90.496L602.496 512L828.8 738.304a64 64 0 0 1-90.496 90.496L512 602.496L285.696 828.8a64 64 0 0 1-90.496-90.496L421.504 512L195.2 285.696a64 64 0 0 1 0-90.496z"
+    /></svg
+  >
+</button>
+
+<style>
+   .exit {
+    grid-area: exit;
+    justify-self: end;
+    background-color: transparent;
+    border: transparent;
+    color: #8b0000;
+    cursor: pointer;
+  }
+</style>
--- src/lib/components/dialogs/End.svelte
+++ src/lib/components/dialogs/End.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { copyPgn, exportToPgn } from "$lib/io";
   import Dialog from "./Dialog.svelte";
+  import ExitButton from "../ExitButton.svelte"
+  import CopyIcon from "../CopyIcon.svelte";
   import { createEventDispatcher } from "svelte";
   import type { GameContext } from "$lib/context";
 
@@ -34,18 +36,7 @@
 </script>
 
 <Dialog class="end-dialog" on:submit={close}>
-  <button class="exit"
-    ><svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="32"
-      height="32"
-      viewBox="0 0 1024 1024"
-      ><path
-        fill="currentColor"
-        d="M195.2 195.2a64 64 0 0 1 90.496 0L512 421.504L738.304 195.2a64 64 0 0 1 90.496 90.496L602.496 512L828.8 738.304a64 64 0 0 1-90.496 90.496L512 602.496L285.696 828.8a64 64 0 0 1-90.496-90.496L421.504 512L195.2 285.696a64 64 0 0 1 0-90.496z"
-      /></svg
-    ></button
-  >
+  <ExitButton/>
 
   <h2 class="title" id="end-title">
     <span class="reason">{$game.terminationReason}!</span>
@@ -60,31 +51,7 @@
     >
     <button class="copy-btn" type="button" on:click={handleCopy}
       >copy pgn <span class="copy-icon">
-        {#if !copied}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="icon"
-            viewBox="0 0 16 16"
-            ><path
-              fill="currentColor"
-              d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"
-            /><path
-              fill="currentColor"
-              d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"
-            /></svg
-          >
-        {:else}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="icon"
-            viewBox="0 0 12 16"
-            ><path
-              fill-rule="evenodd"
-              d="M12 5l-8 8l-4-4l1.5-1.5L4 10l6.5-6.5L12 5z"
-              fill="currentColor"
-            /></svg
-          >
-        {/if}
+        <CopyIcon {copied}/>
       </span></button
     >
   </span>
@@ -169,8 +136,4 @@
     background-color: black;
   }
 
-  .icon {
-    height: var(--element-size);
-    width: var(--element-size);
-  }
 </style>
